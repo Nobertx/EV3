4f596749d24ef179b27b725099e57d6b18a01be8
Nobertx <emmanuel2012123@gmail.com>
2023-06-06 20:52:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,59 @@
 #!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
-from pybricks.parameters import Port
-from pybricks.robotics import DriveBase
+from pybricks.hubs import EV3Brick  # Importa a classe EV3Brick do módulo pybricks.hubs
+from pybricks.ev3devices import Motor  # Importa a classe Motor do módulo pybricks.ev3devices
+from pybricks.parameters import Port  # Importa a classe Port do módulo pybricks.parameters
+from pybricks.robotics import DriveBase  # Importa a classe DriveBase do módulo pybricks.robotics
+import time
+import math  # Importa o módulo math para operações matemáticas
 
-# Inicialização do EV3 Brick
-ev3 = EV3Brick()
+ev3 = EV3Brick()  # Cria uma instância da classe EV3Brick para interagir com o bloco EV3
 
-# Inicialização dos motores
-left_motor = Motor(Port.A)
-right_motor = Motor(Port.D)
+left_motor = Motor(Port.A)  # Cria uma instância da classe Motor para o motor esquerdo
+right_motor = Motor(Port.D)  # Cria uma instância da classe Motor para o motor direito
 
-# Inicialização do DriveBase
-robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+# Configure as medidas da roda
+wheel_diameter = 43.2  # Diâmetro da roda em milímetros
+axle_track = 104  # Distância entre as rodas em milímetros
 
-# Definição da distância em milímetros
-distancia = 200
+# Cria uma instância da classe DriveBase para controlar o movimento do robô
+robot = DriveBase(left_motor, right_motor, wheel_diameter=wheel_diameter, axle_track=axle_track)
 
-# Configuração das velocidades dos motores (em graus por segundo)
-velocidade = 500
+ev3.speaker.beep()  # Emite um som para indicar o início do programa
+robot.straight(1000)  # Move o robô para frente por 1000mm
 
-# Configuração da aceleração dos motores (em graus por segundo ao quadrado)
-aceleracao = 1000
+# Obter as distâncias percorridas pelos motores
+left_motor_distance = left_motor.angle() * math.pi * wheel_diameter / 360  # Calcula a distância percorrida pelo motor esquerdo em milímetros
+right_motor_distance = right_motor.angle() * math.pi * wheel_diameter / 360  # Calcula a distância percorrida pelo motor direito em milímetros
 
-# Configuração da velocidade de rotação (em graus por segundo)
-velocidade_rotacao = 200
+# Calcular a posição aproximada do robô
+distance_center = (left_motor_distance + right_motor_distance) / 2  # Calcula a média das distâncias percorridas pelos motores em milímetros
+angle_rad = math.radians(robot.angle())  # Converte o ângulo do robô de graus para radianos
+x = distance_center * math.cos(angle_rad)  # Calcula a coordenada X da posição em milímetros
 
-# Configuração da aceleração de rotação (em graus por segundo ao quadrado)
-aceleracao_rotacao = 500
-
-# Configuração do tempo de espera para cada movimento (em milissegundos)
-tempo_espera = 1000
-
-# Configuração do tempo de espera para as rotações (em milissegundos)
-tempo_espera_rotacao = 500
-
-# Configuração da frenagem suave
-robot.settings(velocidade, aceleracao, velocidade_rotacao, aceleracao_rotacao)
-
-# Andar em linha reta
-robot.straight(distancia)
-ev3.speaker.beep()
-
-# Virar para a direita
+ev3.speaker.beep()  # Emite um som para indicar o final do movimento para frente
 robot.turn(90)
-ev3.speaker.beep()
+robot.straight(1000)  # Move o robô para trás por 1000mm
 
-# Percorrer a mesma distância
-robot.straight(distancia)
-ev3.speaker.beep()
+# Obter as distâncias percorridas pelos motores
+left_motor_distance = left_motor.angle() * math.pi * wheel_diameter / 360  # Calcula a distância percorrida pelo motor esquerdo em milímetros
+right_motor_distance = right_motor.angle() * math.pi * wheel_diameter / 360  # Calcula a distância percorrida pelo motor direito em milímetros
 
-# Dar meia volta
-robot.turn(180)
-ev3.speaker.beep()
+# Calcular a posição aproximada do robô
+distance_center = (left_motor_distance + right_motor_distance) / 2  # Calcula a média das distâncias percorridas pelos motores em milímetros
+angle_rad = math.radians(robot.angle())  # Converte o ângulo do robô de graus para radianos
+y = distance_center * math.sin(angle_rad)  # Calcula a coordenada Y da posição em milímetros
 
-# Andar o dobro da distância anterior
-robot.straight(distancia * 2)
-ev3.speaker.beep()
+x = round(x, 2)
+y = round(y, 2)
 
-robot.turn(180)
-ev3.speaker.beep()
+# Imprimir a posição atual do robô
+print("Posição atual do robô:")
+print("X: {} mm".format(x))
+print("Y: {} mm".format(y))
+ev3.screen.print("X: {} mm".format(x))
+time.sleep(5)
+ev3.screen.print("Y: {} mm".format(y))
+time.sleep(5)
 
-robot.straight(distancia)
-ev3.speaker.beep()
 # Parar os motores
 robot.stop()
